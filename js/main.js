d28a71eff15f30b204b12277186b725acf46bf7a
Regina Campos <rmscampos@gmail.com>
2019-11-08 00:11:32 -0600

    updatePage functionality

--- js/main.js
+++ js/main.js
@@ -1,78 +1,47 @@
 /*----- constants -----*/
-// global JavaScript variables
 
-//will keep track of the items to diaply on the current page only
-let pageList = [];
-//where we are in the pagination
-let currentPage = 0;
-//amount of objects per page
-let numberPerPage = 1;
-
-// let numberOfPages = 1;   // calculates the total number of pages, maybe not necessary
+let answersContainer = [];
+let currentIdx = 0;
+// let numberPerPage = 1;
 
 let dinosaurs = [
     { 
+        image: './assets/dinoimages/stego.png',
         dinosaur: 'Stegosaurus',
-        answers: { 
-            a: 'Jurassic', 
-            b: 'Cretaceous',  
-            c: 'Triassic'
+        answers: ['Jurassic', 'Cretaceous', 'Triassic'],
+        correctAnswer: 'Jurassic'
         },   
-        correctAnswer: 'a'
-    },
     { 
+        image: './assets/dinoimages/trex.jpeg',
         dinosaur: 'Tyrannosaurus Rex',
-        answers: { 
-            a: 'Jurassic', 
-            b: 'Cretaceous',  
-            c: 'Triassic'
-        },   
-        correctAnswer: 'b'
+        answers: ['Jurassic', 'Cretaceous', 'Triassic'],  
+        correctAnswer: 'Cretaceous'
     },
     { 
+        image: './assets/dinoimages/Apatosaurus',
         dinosaur: 'Apatosaurus',
-        answers: { 
-            a: 'Jurassic', 
-            b: 'Cretaceous',  
-            c: 'Triassic'
-        },   
-        correctAnswer: 'a'
+        answers: ['Jurassic', 'Cretaceous', 'Triassic'], 
+        correctAnswer: 'Jurassic'
     },
     { 
         dinosaur: 'Procompsognathus',
-        answers: { 
-            a: 'Jurassic', 
-            b: 'Cretaceous',  
-            c: 'Triassic'
-        },   
-        correctAnswer: 'c'
+        answers: ['Jurassic', 'Cretaceous', 'Triassic'],  
+        correctAnswer: 'Triassic'
     },
     { 
         dinosaur: 'Brachiosaurus',
-        answers: { 
-            a: 'Jurassic', 
-            b: 'Cretaceous',  
-            c: 'Triassic'
-        },   
-        correctAnswer: 'a'
+        answers: ['Jurassic', 'Cretaceous', 'Triassic'],  
+        correctAnswer: 'Jurassic'
     },
     { 
         dinosaur: 'Velociraptor',
-        answers: { 
-            a: 'Jurassic', 
-            b: 'Cretaceous',  
-            c: 'Triassic'
-        },   
-        correctAnswer: 'b'
+        answers: ['Jurassic', 'Cretaceous', 'Triassic'],   
+        correctAnswer: 'Cretaceous'
     },
     { 
         dinosaur: 'Eoraptor',
-        answers: { 
-            a: 'Jurassic', 
-            b: 'Cretaceous',  
-            c: 'Triassic'
-        },   
-        correctAnswer: 'c'
+        answers: ['Jurassic', 'Cretaceous', 'Triassic'],  
+        correctAnswer: 'Eoraptor'
     },
     { 
         dinosaur: 'Triceratops',
@@ -81,32 +50,23 @@ let dinosaurs = [
     },
     { 
         dinosaur: 'Anklyosaurus',
-        answers: { 
-            a: 'Jurassic', 
-            b: 'Cretaceous',  
-            c: 'Triassic'
-        },
-        correctAnswer: 'a'
+        answers: ['Jurassic', 'Cretaceous', 'Triassic'],
+        correctAnswer: 'Jurassic'
     },
     { 
         dinosaur: 'Iguanodon',
-        answers: { 
-            a: 'Jurassic', 
-            b: 'Cretaceous',  
-            c: 'Triassic'
-        },
-        correctAnswer: 'b'
+        answers: ['Jurassic', 'Cretaceous', 'Triassic'],
+        correctAnswer: 'Cretaceous'
     },
 ];
 
 
 /*----- app's state (variables) -----*/
 
-//score (const scorePercent = Math.round(100 * score/questions.length)
-
 let numCorrect = 0;
 
 /*----- cached element references -----*/
+
 let dinoType = document.querySelector('.dinotype');
 
 let start = document.getElementById('start');
@@ -119,15 +79,13 @@ let replay = document.getElementById('replay');
 let home = document.getElementById('home');
 let quiz = document.getElementById('quiz-container');
 
-
-
-// let answersContainer = quizContainer.querySelectorAll('.answers');
+// let answersContainer = dinosaurs.correctAnswer;
 
 /*----- event listeners -----*/
 
+
 start.addEventListener('click', startGame);
 next.addEventListener('click', nextQuestion);
-previous.addEventListener('click', previousQuestion);
 submit.addEventListener('click', submitAnswers);
 answers.addEventListener('click', viewAnswers);
 replay.addEventListener('click', replayGame);
@@ -135,12 +93,7 @@ replay.addEventListener('click', replayGame);
 window.addEventListener('load', landingPage);
 
 /*----- functions -----*/
-              
-                    
-// section[currentPage/Question];
-//each dino div display = block;
-//from home page, where nothing but home info is displayed, when user clicks start game, the first dinosaur will appear
-                    
+                                  
                 
 function landingPage () {
     quiz.style.display = 'none';
@@ -153,16 +106,8 @@ function landingPage () {
 
 
 
-// function updatePage() {
-//     let currentDino = dinosaurs[currentPage];
-//     dinoType.innerHTML = currentDino.dinosaur;
-// }
-
                                                              
 function startGame() {
-    let currentDino = dinosaurs[currentPage];
-        console.log(currentDino);
-    dinoType.innerHTML = currentDino.dinosaur;
     quiz.style.display = 'block';
     next.style.display = 'block';
     home.style.display = 'none'; 
@@ -173,36 +118,34 @@ function startGame() {
     replay.style.display = 'none';
 };
 
-
-                                
+function updatePage() {
+    let currentDino = dinosaurs[currentIdx];
+    dinoType.innerHTML = currentDino.dinosaur;
+};
+                        
 function nextQuestion() {
     let answer = document.querySelector('input[name="choice"]:checked').value;
     console.log(answer);
-    if (answer === dinosaurs[currentPage].correctAnswer) {
-        console.log('it is right');
-        //correct, add to score, etc
+    if (answer === dinosaurs[currentIdx].correctAnswer) {
+        currentIdx += 1;
+        updatePage();
+        answersContainer.push(dinosaurs.correctAnswer);
     } else {
-        console.log('you wrong');
-        //not correct
+        alert('Wrongo! Guess again!');
     }
-    // go to next dino
-    currentPage += 1;
-    // updatePage();
-};
+    
+    };
 
-
-function previousQuestion(evt) {
-    console.log(evt);
-};
 function submitAnswers(evt) {
     console.log(evt);
 };
-function viewAnswers(evt) {
-    const answersContainer = quizContainer.querySelectorAll(".answers");
+
+function viewAnswers() {
+    ;
 };
 
 
-function replayGame(evt) {
+function replayGame() {
     window.location.reload(true);
 };
 
@@ -212,10 +155,7 @@ function replayGame(evt) {
 //     numCorrect++
 // };
 
-//show results
-// function viewAnswers() {
-//     let answersContainer = quizContainer.querySelector
-// }
+
 //render the question
 //render progress
  
